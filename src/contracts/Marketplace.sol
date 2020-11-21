d1f6d92438794094045c0c2b3ddfa053c5a44f36
Hippolyte THOMAS <hippolyte.thomas1@gmail.com>
2020-11-21 17:58:58 +0100

    Adding Truffle & Marketplace contract

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.7.0;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "HT Marketplace";
+    }
+}
